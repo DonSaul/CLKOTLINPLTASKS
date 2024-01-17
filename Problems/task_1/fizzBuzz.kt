e4aa9ec95b69dd97f40a4fcb436158f7fea9fac4
Santisu <esantibra@gmail.com>
2024-01-17 17:46:18 -0300

    Solved tasks 1

---
+++ Problems/task_1/fizzBuzz.kt
@@ -0,0 +1,29 @@
+/**
+* First of all, for better readability, using the extension functionality I added the
+* methods necesary to check if the given number is divisible by 3, 5 or both.
+* Then, through a "when" statement it verifies wich requirement meets and prints the given value.
+*
+* Usefull learning: 
+* - The ".." operator, that create inclusive ranges, including the max number in the iteration.
+* - The "extension functionality", that allows to add new funtionality to a class without creating a new one
+*   or using decorators.
+*/
+
+fun Int.isDivisibleBy3(): Boolean =  this % 3 == 0 
+fun Int.isDivisibleBy5(): Boolean =  this % 5 == 0 
+fun Int.isDivisibleByBoth(): Boolean = this.isDivisibleBy3() && this.isDivisibleBy5()
+
+fun fizzBuzz(max:Int) {
+   for (i in 1..max) {
+      when {
+         i.isDivisibleByBoth() -> println("$i FizzBuzz")
+         i.isDivisibleBy5() -> println("$i Buzz")
+         i.isDivisibleBy3() -> println("$i Fizz")
+         else -> println(i)
+      }
+   }
+}
+
+fun main() {
+    fizzBuzz(20)
+}
---
+++ Problems/task_1/reverseString.kt
@@ -0,0 +1,21 @@
+/**
+ * To reverse the given input, I have used a mutable list and used it as a stack
+ * iterating through each character of the input I added it to the beggining of the list.
+ * Once all the characters are added to the list, it is already reversed so the only step aside is
+ * to join the list on a String using the .joinToString method.
+ */
+fun reverseString(texto:String): String  {
+    val reversedList: MutableList<Char> = mutableListOf()
+    for (i in 0 until texto.length) {
+        reversedList.addFirst(texto[i])
+    }
+    println(texto) // Prints the input to compare with the output
+    return reversedList.joinToString(separator="") 
+}
+
+
+fun main() {
+    println(reverseString("Rey pirata"))
+}
+
+
