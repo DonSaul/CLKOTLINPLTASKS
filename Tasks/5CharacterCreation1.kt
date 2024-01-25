5d6e75c11daf0fe7e8be5ac9045968186a07c93a
jamirou <devjamiro@gmail.com>
2024-01-24 23:13:22 -0300

    wizard character

--- Tasks/5CharacterCreation1.kt
+++ Tasks/5CharacterCreation1.kt
@@ -11,20 +11,34 @@ class Character(
     }
     fun attack(target: Character) {
         target.takeDamage(attackPoints)
-        println("$name attacked ${target.name} using ${attackPoints} damage.")
+        println("$name attack ${target.name} using ${attackPoints} damage.")
     }
 }
 
 fun main() {
-    val character1 = Character("Thorin Oakshield", 200, 150)
-    val character2 = Character("Kili", 200, 90)
+    val character1 = Character("Kili", 200, 90)
+    val character2 = Character("Thorin Oakshield", 200, 120)
+
 
     println("${character1.name}\nHealth: ${character1.healtPoints}\nAttack: ${character1.attackPoints}")
     println("------VS------")
     println("${character2.name}\nHealth: ${character2.healtPoints}\nAttack: ${character2.attackPoints}")
     println()
-    character1.attack(character2)
-    println("${character1.name}\nHealth: ${character1.healtPoints}\nAttack: ${character1.attackPoints}")
-    println("--------------")
-    println("${character2.name}\nHealth: ${character2.healtPoints}\nAttack: ${character2.attackPoints}")
+    while (character2.healtPoints > 0 && character1.healtPoints > 0) {
+        println("----------------")
+        character1.attack(character2)
+        println("Health: ${character1.healtPoints}")
+        println("--------------")
+        println("${character2.name} was injured\nHealth: ${character2.healtPoints}")
+        println("----------------")
+        character2.attack(character1)
+        println("----------------")
+        println("${character1.name} was injured\nHealth: ${character1.healtPoints}")
+        println("----------------")
+    }
+    if (character1.healtPoints > 0) {
+        println("${character2.name} has been defeated\nThe winner is ${character1.name}")
+    } else {
+        println("${character1.name} has been defeated\nThe winner is ${character2.name}")
+    }
 }
