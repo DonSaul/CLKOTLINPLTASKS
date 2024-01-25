4d721ba45c5088098de70fbcd373e68059cb6bc2
jamirou <devjamiro@gmail.com>
2024-01-24 22:22:22 -0300

    character fight

--- Tasks/5CharacterCreation1.kt
+++ Tasks/5CharacterCreation1.kt
@@ -11,7 +11,20 @@ class Character(
     }
     fun attack(target: Character) {
         target.takeDamage(attackPoints)
-        println("$name attacked ${target.name} for ${attackPoints} damage.")
+        println("$name attacked ${target.name} using ${attackPoints} damage.")
     }
 }
 
+fun main() {
+    val character1 = Character("Thorin Oakshield", 200, 150)
+    val character2 = Character("Kili", 200, 90)
+
+    println("${character1.name}\nHealth: ${character1.healtPoints}\nAttack: ${character1.attackPoints}")
+    println("------VS------")
+    println("${character2.name}\nHealth: ${character2.healtPoints}\nAttack: ${character2.attackPoints}")
+    println()
+    character1.attack(character2)
+    println("${character1.name}\nHealth: ${character1.healtPoints}\nAttack: ${character1.attackPoints}")
+    println("--------------")
+    println("${character2.name}\nHealth: ${character2.healtPoints}\nAttack: ${character2.attackPoints}")
+}
