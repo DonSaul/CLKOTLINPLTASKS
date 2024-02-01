bb95e7b719a3813af4a4478227a78fbb4106c41b
Santisu <esantibra@gmail.com>
2024-02-01 00:48:03 -0300

    add army and tests

--- src/main/kotlin/projectlab/Main.kt
+++ src/main/kotlin/projectlab/Main.kt
@@ -2,7 +2,7 @@ package org.softserve.projectlab
 
 /**
  * Simula un duelo entre dos objetos de la clase Warrior.
- * Retorna true si el primer argumento gana el duelo, false en caso contrario.
+ * @return true si el primer argumento gana el duelo, false en caso contrario.
  *
  * @param warrior1 objeto de clase Warrior.
  * @param warrior2 objeto de clase Warrior.
@@ -17,15 +17,52 @@ fun fight(warrior1: Warrior,
     }
     return warrior1.isAlive
 }
-fun main() {
-    val warrior1 = Warrior()
-    val warrior2 = Knight()
-    println(warrior1.isAlive)
-    val result = fight(warrior1, warrior2)
-    if (result) {
-        println("Warrior 1 wins!")
-    } else {
-        println("Warrior 2 wins!")
+
+/**
+ * Simula un combate entre dos ejercitos.
+ *
+ * Se obtiene el primer objeto de la clase Warrior de cada Army.
+ *
+ * El loop se ejecutará siempre y cuando ambas Army tengan Warriors vivos para luchar,
+ * en cada ciclo hay un duelo entre el Warrior de turno de cada Army,
+ * si un Warrior es derrotado se obtiene el siguiente de su Army para continuar la lucha.
+ *
+ * @param army1 Ejército que combatirá
+ * @param army2 Ejército que combatirá
+ * @return true si el army1 es el ganador y false si pierde
+ *
+ */
+fun fight(army1: Army,
+          army2: Army): Any {
+    var warrior1: Warrior = army1.nextFighter()
+    var warrior2: Warrior = army2.nextFighter()
+
+    while (army1.hasFighters() && army2.hasFighters() ) {
+        val warrior1Win: Boolean = fight(warrior1, warrior2)
+        if (warrior1Win && army2.hasFighters()) {
+            warrior2 = army2.nextFighter()
+        } else if (!warrior1Win && army1.hasFighters()){
+            warrior1 = army1.nextFighter()
+        }
     }
-    println(warrior1.isAlive)
+    return army1.hasFighters()
+}
+
+fun main() {
+    val myArmy = Army()
+    myArmy.addUnits(2) { Warrior() }
+
+    val enemyArmy = Army()
+    enemyArmy.addUnits(3) { Warrior() }
+
+    val army3 = Army()
+    army3.addUnits(20) { Warrior() }
+    army3.addUnits(5) { Knight() }
+
+    val army4 = Army()
+    army4.addUnits(30) { Warrior() }
+    check(fight(myArmy, enemyArmy) == true)
+    check(fight(army3, army4) == false)
+    println("OK")
+
 }
---
+++ src/test/kotlin/projectlab/BattleArmyTest.kt
@@ -0,0 +1,94 @@
+package org.softserve.projectlab
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+
+class BattleArmyTest {
+
+    @Test
+    @DisplayName("1. Battle")
+    fun `Army2 debe ganar a Army1`() {
+        // given
+        val army1 = Army()
+        val army2 = Army()
+        army1.addUnits(1) { Warrior() }
+        army2.addUnits(2) { Warrior() }
+        // when
+        val res = fight(army1, army2)
+        // then
+        assertEquals(false, res)
+    }
+
+    @Test
+    @DisplayName("2. Battle")
+    fun `Army2 debe ganar a Army1 p2`() {
+        // given
+        val army1 = Army()
+        val army2 = Army()
+        army1.addUnits(2) { Warrior() }
+        army2.addUnits(3) { Warrior() }
+        // when
+        val res = fight(army1, army2)
+        // then
+        assertEquals(false, res)
+    }
+
+    @Test
+    @DisplayName("3. Battle")
+    fun `Army2 debe ganar a Army1 p3`() {
+        // given
+        val army1 = Army()
+        val army2 = Army()
+        army1.addUnits(5) { Warrior() }
+        army2.addUnits(7) { Warrior() }
+        // when
+        val res = fight(army1, army2)
+        // then
+        assertEquals(false, res)
+    }
+
+    @Test
+    @DisplayName("4. Battle")
+    fun `Army1 debe ganar a Army2`() {
+        // given
+        val army1 = Army()
+        val army2 = Army()
+        army1.addUnits(20) { Warrior() }
+        army2.addUnits(21) { Warrior() }
+        // when
+        val res = fight(army1, army2)
+        // then
+        assertEquals(true, res)
+    }
+
+    @Test
+    @DisplayName("5. Battle")
+    fun `Army1 debe ganar a Army2 p2`() {
+        // given
+        val army1 = Army()
+        val army2 = Army()
+        army1.addUnits(10) { Warrior() }
+        army2.addUnits(11) { Warrior() }
+        // when
+        val res = fight(army1, army2)
+        // then
+        assertEquals(true, res)
+    }
+
+    @Test
+    @DisplayName("6. Battle")
+    fun `Army1 debe ganar a Army2 p3`() {
+        // given
+        val army1 = Army()
+        val army2 = Army()
+        army1.addUnits(11) { Warrior() }
+        army2.addUnits(7) { Warrior() }
+        // when
+        val res = fight(army1, army2)
+        // then
+        assertEquals(true, res)
+    }
+
+}
